45e55c5e691f08dc70cc069a37bd4851d2a26b53
Daniel Kukula <daniel.kuku@gmail.com>
2017-10-18 20:33:03 +0100

    Adding new todos on text input

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,26 @@
 //Check off specific tofos by clocking
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 //Check if X is clicked, if yes then delete
-$("span").on("click", function (event) {
+$("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
-})
+});
+//Add new todo on text input enter click
+var delButton = "<span>X</span> ";
+
+$("input[type='text']").keypress(function (e) {
+    if(e.which === 13)  // the enter key code
+     {
+        // save text to variable
+        var todoText = "<li>" + delButton + $(this).val() + "</li>";
+        // clear input text
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append(todoText);
+     }
+   });
+
