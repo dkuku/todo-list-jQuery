a404a384688513fbfc42d55f5d319f33fbe5da42
Daniel Kukula <daniel.kuku@gmail.com>
2017-10-19 22:48:23 +0100

    Moved all todos to localStorage

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,35 +2,60 @@ var name;
 //Check off specific todos by clicking
 $("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
+    // change if completed in local storage
+    var toCheckOff = $(this).text().slice(1);
+    if ($(this).hasClass("completed")) {
+        window.localStorage.setItem(toCheckOff, "true");
+    } else {
+        window.localStorage.setItem(toCheckOff, "false");
+    }
+
 });
 //Check if X is clicked, if yes then delete
 $("ul").on("click", "span", function (event) {
     $(this).parent().fadeOut(500, function() {
+        var toDelete = $(this).text().slice(1);
+        //remove from localStorage
+        window.localStorage.removeItem(toDelete);
+        //demove from list`
         $(this).remove();
     });
     event.stopPropagation();
 });
-//Add new todo on text input enter click
-var delButton = "<span><i class='fa fa-trash' aria-hidden='true'></i></span> ";
 
+//Add new todo on text input enter click
 $("input[type='text']").keypress(function (e) {
     if(e.which === 13)  // the enter key code
      {
         // save text to variable
-        var todoText = "<li>" + delButton + $(this).val() + "</li>";
+        var todoText = $(this).val()
+        addToList(todoText);
         // clear input text
         $(this).val("");
-        // create a new li and add to ul
-        $("ul").append(todoText);
-     }
-   });
+        //add entry to localStorage
+        window.localStorage.setItem(todoText, "false");
+    }    
+});
 
+function addToList (todoText, completed) {
+        // generate text button
+        var delButton = "<span><i class='fa fa-trash' aria-hidden='true'></i></span> ";
+        if (completed === "true") {
+            var listEntry = "<li class='completed'>" + delButton + todoText + "</li>";
+        } else {
+            var listEntry = "<li>" + delButton + todoText + "</li>";
+        }
+        // create a new li and add to ul
+        $("ul").append(listEntry);
+}
+
+//hide add todo input
 $(".fa-plus").on("click", function () {
     $("input[type='text']").fadeToggle(500);    
 });
+
 //on first load check if we have stored name if not then ask for it and display
 window.onload = function () {
-    console.log(window.localStorage);
     if (!window.localStorage.getItem("name")) {
         name = prompt("Enter your name");
         window.localStorage.setItem("name", name);
@@ -38,4 +63,12 @@ window.onload = function () {
         name = window.localStorage.getItem("name");
     }
     $("h1").prepend(name + "'s ")
+
+    //restore saved todos from localStorage
+    $.each(localStorage, function(todo, completed){
+        if (!(todo === "name")) {
+            addToList(todo, completed);
+        }
+    });
+
 };
